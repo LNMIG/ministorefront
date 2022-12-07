9123f24517067f2096a6012fa9938e6a095ea4de
LNMIG <migliorenicolas@yahoo.com.ar>
2022-12-07 16:48:20 -0300

    FIX: trying to fix CORS problems

--- api/index.js
+++ api/index.js
@@ -18,11 +18,12 @@ const typeDefs = readFileSync( join( './', 'src/schema', 'schema.gql'),'utf-8' )
 
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 
-app.use(cors())
+//app.use(cors())
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
 })
 app.use('/api', graphqlHTTP({
