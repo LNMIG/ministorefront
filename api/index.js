fbf382e2bf2e3655369c21aed203e6120e95bb4b
LNMIG <migliorenicolas@yahoo.com.ar>
2022-12-07 17:05:00 -0300

    FIX: trying to fix CORS problems adding HTTP ok status

--- api/index.js
+++ api/index.js
@@ -24,6 +24,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('HTTP/1.1 200 OK');
     next();
 })
 app.use('/api', graphqlHTTP({
